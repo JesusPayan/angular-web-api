81d6130c3540941d76bede3e45ccdd8224b3d232
JesusArmandoHernandezPayan <jesus.he.payan@gmail.com>
2021-08-10 19:16:49 -0600

    create basic page top navbar and generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BiografyComponent } from './biografy/biografy.component';
+import { BandComponent } from './band/band.component';
+import { DiscografyComponent } from './discografy/discografy.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { StoreComponent } from './store/store.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { ContactComponent } from './contact/contact.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BiografyComponent,
+    BandComponent,
+    DiscografyComponent,
+    GalleryComponent,
+    StoreComponent,
+    ScheduleComponent,
+    ContactComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/discografy/discografy.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-discografy',
+  templateUrl: './discografy.component.html',
+  styleUrls: ['./discografy.component.css']
+})
+export class DiscografyComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
