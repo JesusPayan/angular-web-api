271c371eabf9390853dc1e0986e9ab4cc1b0ef70
JesusArmandoHernandezPayan <jesus.he.payan@gmail.com>
2021-08-11 12:14:24 -0600

    adding routing to the page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BandComponent } from './band/band.component';
+import { BiografyComponent } from './biografy/biografy.component';
+import { ContactComponent } from './contact/contact.component';
+import { DiscografyComponent } from './discografy/discografy.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { HomeComponent } from './home/home.component';
+import { ScheduleComponent } from './schedule/schedule.component';
+import { StoreComponent } from './store/store.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path:'biografy',component:BiografyComponent},
+  {path:'band',component:BandComponent},
+  {path:'discografy',component:DiscografyComponent},
+  {path:'gallery',component:GalleryComponent},
+  {path:'store',component:StoreComponent},
+  {path:'schedule',component:ScheduleComponent},
+  {path:'contact',component:ContactComponent},
+  {path:'**',pathMatch:'full',redirectTo:''},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
